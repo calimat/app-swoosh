14658495c84f8bd4ff1d22cafe4de81b27ebfafd
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2017-07-30 18:29:53 -0500

    Connected controllers to code

--- app-swoosh/Controller/LeagueVC.swift
+++ app-swoosh/Controller/LeagueVC.swift
@@ -11,11 +11,14 @@ import UIKit
 class LeagueVC: UIViewController {
 
 
+    var player: Player!
     
+    @IBOutlet weak var nextBtn: BorderButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        player = Player()
         // Do any additional setup after loading the view.
     }
 
@@ -24,8 +27,22 @@ class LeagueVC: UIViewController {
         performSegue(withIdentifier: "skillVCSegue", sender: self)
         
     }
- 
-
+    @IBAction func onMensTapped(_ sender: Any) {
+        selectLeague(leagueType: "mens")
+    }
+    
+    @IBAction func onWomensTapped(_ sender: Any) {
+        selectLeague(leagueType: "womens")
+    }
+    @IBAction func onCoedTapped(_ sender: Any) {
+        selectLeague(leagueType: "coed")
+    }
+    
+    func selectLeague(leagueType:String) {
+        player.desiredLeague = leagueType
+        nextBtn.isEnabled = true
+    }
+    
     /*
     // MARK: - Navigation
 
---
+++ app-swoosh/Model/Player.swift
@@ -0,0 +1,15 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Ricardo Herrera Petit on 7/28/17.
+//  Copyright © 2017 Ricardo Herrera Petit. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague:String?
+    var selectedSkillLevel: String?
+    
+}
